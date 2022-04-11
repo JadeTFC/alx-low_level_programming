962a0448cc0d8facead813cba96e8c13619c1ad8
JadeTFC <anointedfaithafolayan@gmail.com>
2022-04-11 15:58:55 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: Print letters in reverse.
+ *
+ * Return : 0
+ */
+int main(void)
+{
+
+char ac;
+char ASCII ='\n';
+for (ac = 'z'; ac >= 'a'; ac++)
+{
+putchar(ac);
+}
+putchar(ASCII);
+return (0);
+}
