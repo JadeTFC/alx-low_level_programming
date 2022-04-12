f4f3121822dbf9f0556ae45806f0f8d21655b4c9
JadeTFC <anointedfaithafolayan@gmail.com>
2022-04-12 10:28:29 -0700

    task 0 and task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Decription: Print alphabets in lowercase.
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char d;
+for (d = 'a'; d <= 'z'; d++)
+_putchar(d);
+_putchar('\n');
+}
