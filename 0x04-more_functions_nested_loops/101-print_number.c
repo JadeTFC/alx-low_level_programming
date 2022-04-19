4b3f93eeae5aded805ed681e843480fdd1cc0f4a
JadeTFC <101394926+JadeTFC@users.noreply.github.com>
2022-04-19 15:07:18 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,27 @@
-#include "main.h"
-/**
- * print_number - prints an integer
- * @n:.input integer parameter
- *
- *
- */
-void print_number(int n)
-{
-	unsigned int i = n;
-
-	if (n < 0)
-	{
-		_putchar(45);
-		i = -i;
-	}
-	if (i / 10)
-	{
-		print_number(i / 10);
-	}
-	_putchar(i % 10 + '0');
+#include "main.h"                                                                                                           
+                                                                                                                            
+/**                                                                                                                         
+ * print_number - prints an integer n                                                                                       
+ * @n: integer to be printed                                                                                                
+ */                                                                                                                         
+void print_number(int n)                                                                                                    
+{                                                                                                                           
+        unsigned int n1;                                                                                                    
+                                                                                                                            
+        if (n < 0)                                                                                                          
+        {                                                                                                                   
+                n1 = -n;                                                                                                    
+                _putchar('-');                                                                                              
+        }                                                                                                                   
+        else                                                                                                                
+        {                                                                                                                   
+                n1 = n;                                                                                                     
+        }                                                                                                                   
+                                                                                                                            
+        if (n1 / 10)                                                                                                        
+        {                                                                                                                   
+                print_number(n1 / 10);                                                                                      
+        }                                                                                                                   
+                                                                                                                            
+        _putchar((n1 % 10) + '0');                                                                                          
 }
